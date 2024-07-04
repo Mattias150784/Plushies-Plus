1256b6f8adbab9d0c26eae06489f6cbe520b4b03
jason13official <jason13official@gmail.com>
2024-07-04 08:38:12 -0400

    item drop + gui display fix

--- fabric/src/main/java/com/mattias/plushies_plus/core/PlushieBlocksFabric.java
+++ fabric/src/main/java/com/mattias/plushies_plus/core/PlushieBlocksFabric.java
@@ -12,10 +12,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
 
 public class PlushieBlocksFabric {
 
-    public static final Block ZOMBIE_PLUSHIE = registerBlock("zombie_plushie", new PlushieBlock(SoundEvents.ZOMBIE_AMBIENT, BlockBehaviour.Properties.of().strength(0.8F).noOcclusion().sound(SoundType.WOOL).ignitedByLava()));
+    public static final Block ZOMBIE_PLUSHIE = registerBlock("zombie_plushie", new PlushieBlock(SoundEvents.ZOMBIE_AMBIENT, BlockBehaviour.Properties.of().strength(0.5F).noOcclusion().sound(SoundType.WOOL).ignitedByLava().forceSolidOn().pushReaction(PushReaction.DESTROY)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
